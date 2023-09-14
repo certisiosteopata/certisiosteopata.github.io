784705212ffbda3b71717a9a4ade620c245541a1
Edoardo Alberti <edoalberti99@gmail.com>
2023-09-15 00:23:10 +0200

    fixed ui

--- assets/src/app.js
+++ assets/src/app.js
@@ -1,6 +1,9 @@
 import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
 
 window.Apline = Alpine;
+
+Alpine.plugin(intersect);
 
 Alpine.data('handle_menu_hover', (selected, active) => ({
    active,
@@ -11,7 +14,10 @@ Alpine.data('handle_menu_hover', (selected, active) => ({
    mouseLeave() {
       this.selected = active;
    },
-   mouseClick(newActive) {this.active = newActive}
+   mouseClick(newActive) {
+      this.selected = -1;
+      this.active = newActive;
+   }
 }));
 
 Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,50 @@ module.exports = {
    content: ['content/**/*.md', 'layouts/**/*.html'],
    darkMode: 'class',
    theme: {
-      extend: {}
+      extend: {},
+      screens: {
+         sm: '900px',
+         md: '768px',
+         lg: '1024px',
+         xl: '1280px',
+         '2xl': '1536px'
+      },
+      fontFamily: {
+         body: [
+            'Inter',
+            'ui-sans-serif',
+            'system-ui',
+            '-apple-system',
+            'system-ui',
+            'Segoe UI',
+            'Roboto',
+            'Helvetica Neue',
+            'Arial',
+            'Noto Sans',
+            'sans-serif',
+            'Apple Color Emoji',
+            'Segoe UI Emoji',
+            'Segoe UI Symbol',
+            'Noto Color Emoji'
+         ],
+         sans: [
+            'Inter',
+            'ui-sans-serif',
+            'system-ui',
+            '-apple-system',
+            'system-ui',
+            'Segoe UI',
+            'Roboto',
+            'Helvetica Neue',
+            'Arial',
+            'Noto Sans',
+            'sans-serif',
+            'Apple Color Emoji',
+            'Segoe UI Emoji',
+            'Segoe UI Symbol',
+            'Noto Color Emoji'
+         ]
+      }
    },
    plugins: []
 };
